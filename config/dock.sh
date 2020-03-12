45fb26f2c5395f91eed302d56c858e303f67325f
Adam Gastineau <adam@appcannonsoftware.com>
2020-03-11 20:40:44 -0700

    Updated incorrect Dock paths

--- config/dock.sh
+++ config/dock.sh
@@ -1,13 +1,13 @@
 dockIcons=(
     /Applications/Safari.app
     /Applications/Firefox.app
-    /Applications/Mail.app
-    /Applications/Messages.app
-    /Applications/Calendar.app
-    /Applications/Music.app
-    "/Applications/App Store.app"
-    "/Applications/System Preferences.app"
-    /Applications/Utilities/Terminal.app
+    /System/Applications/Mail.app
+    /System/Applications/Messages.app
+    /System/Applications/Calendar.app
+    /System/Applications/Music.app
+    "/System/Applications/App Store.app"
+    "/System/Applications/System Preferences.app"
+    /System/Applications/Utilities/Terminal.app
     /Applications/Xcode.app
     "/Applications/Visual Studio Code.app"
 )
